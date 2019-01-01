1b874010ac10127988c3237abec2a59c9742beb1
weekbin <18717744742@163.com>
2019-01-02 00:10:10 +0800

    init search-bar

--- src/components/search-bar/search-block.vue
+++ src/components/search-bar/search-block.vue
@@ -1,13 +1,31 @@
 <template>
-	<span>search-block</span>
+	<div>
+		<input class="search" placeholder="Search Headlines..." v-model="search_message">
+	</div>
 </template>
 
 <script>
 	export default {
-		name: "search-block"
+		name: "search-block",
+		data(){
+			return{
+				search_message:null,
+			}
+		}
 	}
 </script>
 
 <style scoped>
-
+.search{
+	box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+	border-radius: 2px;
+	width: 100%;
+	min-height: 48px;
+	display: flex;
+	position: relative;
+	font-family: inherit;
+	margin: 4px 0 24px;
+	padding: 5px 0 0 20px;
+	border: none;
+}
 </style>
--- src/components/search-bar/search-options.vue
+++ src/components/search-bar/search-options.vue
@@ -1,13 +1,47 @@
 <template>
-	<span>search-options</span>
+	<div>
+		<div class="options">
+			<span>All Sites</span>
+			<span></span>
+		</div>
+		<div class="options">
+			<span>Any time</span>
+		</div>
+		<div class="options-chips">
+			<md-chip class="md-primary" @click="deleteChips(index)" v-for="(chip, index) in projects" :key="chip" md-deletable>{{ chip }}</md-chip>
+		</div>
+	</div>
 </template>
 
 <script>
 	export default {
-		name: "search-options"
+		name: "search-options",
+		currentProject: 'Vue Material',
+		data(){
+			return{
+				projects: [
+					'Vue Material',
+					'Element UI',
+					'Quasar',
+					'Axios',
+					'Material Design'
+				]
+			}
+		},
+		methods:{
+			deleteChips(index){
+				this.projects.splice(index, 1)
+			}
+		}
 	}
 </script>
 
 <style scoped>
-
+.options{
+	display: inline-block;
+	margin: 0 20px 15px 5px;
+}
+.options-chips{
+	margin: 0 20px 15px 0;
+}
 </style>
