cae9753f9c3b97c8ed5573f2279ba67a365bbfee
weekbin <18717744742@163.com>
2019-01-02 18:44:33 +0800

    npm install axios

--- src/components/publisher-block.vue
+++ src/components/publisher-block.vue
@@ -9,6 +9,7 @@
 
 <script>
 	import PublisherCard from "@/components/publisher-block/publisher-card";
+	import axios from 'axios'
 	export default {
 		name: "publisher-block",
 		components: {PublisherCard},
@@ -18,7 +19,7 @@
 				{
 					id:1,
 					sites:"豆瓣",
-					list:[
+					all_news:[
 						{id:1, title:"机智的weekbin又写了一个网站机智的weekbin又写了一个网站机智的weekbin又写了一个网站"},
 						{id:2, title:"机智的weekbin组件化写的非常漂亮"},
 						{id:3, title:"机智的weekbin写的代码就艺术品"},
@@ -27,7 +28,7 @@
 				{
 					id:2,
 					sites:"知乎",
-					list:[
+					all_news:[
 						{id:1, title:"还有三分钟就要睡觉了"},
 						{id:2, title:"结果并没有按时睡觉"},
 					]
@@ -35,7 +36,7 @@
 				{
 					id:3,
 					sites:"斗鱼",
-					list:[
+					all_news:[
 						{id:1, title:"早上起来奋斗"},
 						{id:2, title:"写代码是没有睡眠的"},
 					]
@@ -43,7 +44,7 @@
 				{
 					id:4,
 					sites:"郝轲打炉石",
-					list:[
+					all_news:[
 						{id:1, title:"早上起来奋斗"},
 						{id:2, title:"写代码是没有睡眠的"},
 					]
@@ -51,19 +52,31 @@
 				{
 					id:5,
 					sites:"丁丁打英雄联盟",
-					list:[
+					all_news:[
 						{id:1, title:"早上起来奋斗"},
 						{id:2, title:"写代码是没有睡眠的"},
 					]
 				}
 			]
 			}
+		},
+		methods:{
+			publish(){
+				axios.get("/News").then(response=>{
+					this.publishList = response.data.publishList
+				})
+			}
+		},
+		mounted:function () {
+			this.publish()
 		}
 	}
 </script>
 
 <style scoped>
 .publisher-block{
+	position: relative;
+	overflow: hidden;
 	height: 400px;
 	padding: 8px;
 }
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import axios from 'axios'
 
 Vue.use(VueMaterial)
 Vue.config.productionTip = false
+axios.defaults.baseURL = "http://127.0.0.1:5000/api/v0"
 
 new Vue({
   router,
